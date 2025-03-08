8fe43cbd63c955255f5053314e4a3ef53e9e4616
Katsuhiro Kakoi <katsuhiro.k1215@gmail.com>
2025-03-08 16:48:09 +0900

    Update questionnary factory

--- database/factories/QuestionnaryFactory.php
+++ database/factories/QuestionnaryFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Questionnaire;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -9,6 +10,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class QuestionnaryFactory extends Factory
 {
+    protected $model = Questionnaire::class;
     /**
      * Define the model's default state.
      *
@@ -17,7 +19,13 @@ class QuestionnaryFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->name(),
+            'description' => $this->faker->text(),
+            'status' => $this->faker->randomElement(['draft', 'review', 'published', 'paused', 'archived']),
+            'publish_at' => $this->faker->dateTime(),
+            'unpublish_at' => $this->faker->dateTime(),
+            'created_by' => 1,
+            'updated_by' => 1,
         ];
     }
 }
