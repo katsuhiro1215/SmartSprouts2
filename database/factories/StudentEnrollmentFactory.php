c0017b2e55430bd61ffc3956409ff8029277224f
Katsuhiro Kakoi <katsuhiro.k1215@gmail.com>
2025-03-08 16:49:31 +0900

    Update student enrollment factory

--- database/factories/StudentEnrollmentFactory.php
+++ database/factories/StudentEnrollmentFactory.php
@@ -2,22 +2,32 @@
 
 namespace Database\Factories;
 
+use App\Models\Student;
+use App\Models\StudentEnrollment;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\StudentEnrollment>
- */
 class StudentEnrollmentFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+    protected $model = StudentEnrollment::class;
+
     public function definition(): array
     {
         return [
-            //
+            'student_id' => Student::all()->random()->id,
+            'application_date' => $this->faker->dateTimeBetween('2023-05-01', '2024-12-31'),
+            'enrollment_date' => $this->faker->dateTimeBetween('2023-05-01', '2024-12-31'),
+            'selected_days' => $this->faker->randomElement(['月曜日', '火曜日', '木曜日', '金曜日', '土曜日', '日曜日']),
+            'preferred_days' => $this->faker->randomElement(['月曜日', '火曜日', '木曜日', '金曜日', '土曜日', '日曜日']),
+            'status' => $this->faker->randomElement(['承認済み', '承認待ち', 'キャンセル']),
+            'is_paid' => $this->faker->boolean(),
+            'is_notified' => $this->faker->boolean(),
+            'cancel_date' => null,
+            'suspension_start_date' => null,
+            'suspension_end_date' => null,
+            'suspension_reason' => null,
+            'withdrawal_date' => null,
+            'withdrawal_reason' => null,
+            'remarks' => $this->faker->text(),
         ];
     }
 }
