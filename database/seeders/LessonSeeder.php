e90bf1dee74cf6176b64174dd007d88fbd01e9cb
Katsuhiro Kakoi <katsuhiro.k1215@gmail.com>
2025-03-09 01:31:17 +0900

    Update lesson seeder

--- database/seeders/LessonSeeder.php
+++ database/seeders/LessonSeeder.php
@@ -4,14 +4,27 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class LessonSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        DB::table('lessons')->insert([
+            [
+                'store_id' => 1,
+                'name' => '体操教室',
+                'description' => '会員制の体操教室です。',
+                'lesson_photo_path' => null,
+                'status' => true,
+            ],
+            [
+                'store_id' => 1,
+                'name' => 'ヨガ・ピラティス教室',
+                'description' => '会員制の体操教室です。',
+                'lesson_photo_path' => null,
+                'status' => false,
+            ],
+        ]);
     }
 }
