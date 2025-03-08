d5d73bc4c6aa865c0c1e06fc30f98eecf7899ad4
Katsuhiro Kakoi <katsuhiro.k1215@gmail.com>
2025-03-09 01:47:10 +0900

    Update auth layout js resources

---
+++ resources/js/Layouts/AuthLayout.vue
@@ -0,0 +1,20 @@
+<script setup>
+import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+import { Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <div class="min-h-screen flex flex-col justify-center items-center space-y-4 dark:bg-gray-900">
+        <h1>
+            <Link href="/">
+                <ApplicationLogo class="w-20 h-20 fill-current text-gray-500" />
+            </Link>
+        </h1>
+
+        <div
+            class="w-full sm:max-w-md p-6 bg-white dark:bg-slate-700 shadow-md overflow-hidden sm:rounded-lg"
+        >
+            <slot />
+        </div>
+    </div>
+</template>
