a069bbaf897fba8b77b92a4ee0faa39a8fde3314
Katsuhiro Kakoi <katsuhiro.k1215@gmail.com>
2025-03-08 16:45:01 +0900

    Update guardian factory

--- database/factories/GuardianFactory.php
+++ database/factories/GuardianFactory.php
@@ -2,22 +2,30 @@
 
 namespace Database\Factories;
 
+use App\Models\Guardian;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Guardian>
- */
 class GuardianFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+
+    protected $model = Guardian::class;
+
     public function definition(): array
     {
         return [
-            //
+            'lastname' => $this->faker->lastName(),
+            'firstname' => $this->faker->firstName(),
+            'lastname_kana' => $this->faker->lastKanaName(),
+            'firstname_kana' => $this->faker->firstKanaName(),
+            'relationship' => $this->faker->randomElement(['父親', '母親', '兄弟', '親戚', '友達']),
+            'guardian_photo_path' => $this->faker->imageUrl(),
+            'birth' => fake()->dateTimeBetween('1960-01-01', '2000-12-31'),
+            'gender' => $this->faker->randomElement(['男性', '女性']),
+            'mobile_phone_number' => $this->faker->phoneNumber(),
+            'company_name' => $this->faker->company(),
+            'company_phone_number' => $this->faker->phoneNumber(),
+            'is_temporary' => $this->faker->boolean(30),
         ];
     }
 }
